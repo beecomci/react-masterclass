075990ea19d017c2871f0c76ccd689f289e6fc8a
Youjin Park <beecomci@gmail.com>
2022-01-04 00:23:59 +0900

    styled 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border-radius: 15px;
+  border: 0;
+`;
+
+const Input = styled.input.attrs({ required: true, minlenght: 10 })`
+  background-color: yellow;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father as="header">
+      <Box bgColor="teal">
+        <Btn>Log in</Btn>
+        <Btn as="a" href="#">
+          Log in
+        </Btn>
+      </Box>
+      <Circle bgColor="tomato"></Circle>
+      <Input />
+      <Input />
+      <Input />
+    </Father>
   );
 }
 

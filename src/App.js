02ba258ff3a5c3c0368defba5d496733099abacf
Youjin Park <beecomci@gmail.com>
2022-01-04 23:18:08 +0900

    ThemeProvider 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
 `;
 
 const rotationAnimation = keyframes`
@@ -36,9 +45,7 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Text as="p">happy</Text>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
